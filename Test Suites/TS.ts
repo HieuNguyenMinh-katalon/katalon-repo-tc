2b91ba4464ed94a7bead25717e7a3972c477aaa1
hieu.mnguyen+1@katalon.com <katalon_g3_dummy_username>
2025-09-15 08:21:15 +0000

    ok

---
+++ Test Suites/TS.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+    <name>TS</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>3</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>true</rerunImmediately>
+    <testSuiteGuid>ea4bd64e-ba4d-49df-ae9c-9371221c4e46</testSuiteGuid>
+    <testCaseLink>
+        <guid>69c1c386-0b84-4dd7-b37a-da263659205f</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/TestOps/APIs/Autonomous/TC2</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+    </testCaseLink>
+</TestSuiteEntity>
